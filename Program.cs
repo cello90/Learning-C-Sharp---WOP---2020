f4b42782936cfd040f96c2053bd8fe9c658c1da3
cello90 <zac.menzer@gmail.com>
2020-03-19 11:14:35 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Learning_C_Sharp___WOP___2020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
